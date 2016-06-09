ef2426754857a784a91a2956b9073e7cf666b909
Gustavo Aguiar <gugahoa@gmail.com>
2016-06-09 11:09:54 -0300

    CellPos struct and operators, plus tests

--- Maze/src/maze/proceduralmaze.cpp
+++ Maze/src/maze/proceduralmaze.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 #include "proceduralmaze.h"
+
+int main(void)
+{
+	CellPos cell1(5, 5);
+	CellPos cell2(5, 7);
+	CellPos cell3 = cell1 - cell2;
+
+	std::cout << cell1.x << " " << cell1.y << std::endl;
+	std::cout << cell2.x << " " << cell2.y << std::endl;
+	std::cout << cell3.x << " " << cell3.y << std::endl;
+
+	CellPos cell4 = cell3;
+	std::cout << (cell4 == cell3 ? "true" : "false") << std::endl;
+	return 0;
+}
--- Maze/src/maze/proceduralmaze.h
+++ Maze/src/maze/proceduralmaze.h
@@ -1,6 +1,26 @@
 #ifndef __PROCEDURALMAZE_H__
 #define __PROCEDURALMAZE_H__
 
+struct CellPos
+{
+	int x;
+	int y;
 
+	CellPos(int x = 0, int y = 0) : x(x), y(y) {}
+
+	CellPos operator-(const CellPos& other)
+	{
+		CellPos result;
+		result.x = (other.x - x)/2 + x;
+		result.y = (other.y - y)/2 + y;
+
+		return result;
+	}
+
+	bool operator==(const CellPos& other)
+	{
+		return (x == other.x && y == other.y);
+	}
+};
 
 #endif
